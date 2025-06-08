4d4749bcb79314623ec2e3501caf10bb757b998b
dennisaguilar1989@gmail.com <dennisaguilar1989@gmail.com>
2025-06-08 18:14:48 -0500

    Added tests for home page interactions. Checked the drop-down list for list items.

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -20,6 +20,52 @@ it('Home Page renders properly', () => {
 });
 
 it('Home Page interactions work', () => {
+    //Test top-left drop-down menu.
     cy.contains('example.com')
         .should('exist');
+
+    cy.contains('example.com')
+        .click();
+
+    cy.contains('Pages...')
+        .should('exist');
+
+    cy.contains('Users...')
+        .should('exist');
+
+    cy.contains('Administration...')
+        .should('exist');
+
+    cy.contains('Aliases...')
+        .should('exist');
+
+    cy.contains('User settings...')
+        .should('exist');
+
+    cy.contains('Disable toolbar')
+        .should('exist');
+
+    cy.contains('Shortcuts...')
+        .should('exist');
+
+    cy.contains('Logout daguilar')
+        .should('exist');
+    
+    cy.contains('example.com')
+        .click();
+
+    //Test Background Color
+    cy.get('[data-rel=color-toggle]')
+        .click();
+
+    cy.get('.cms-welcome-bg')
+      .should('have.css', 'background-color', 'rgb(68, 68, 68)');
+
+    cy.get('[data-rel=color-toggle]')
+        .click();
+});
+
+it.only('Add page modal', () => {
+    cy.get('.cms-btn-action')
+        .click();
 });
